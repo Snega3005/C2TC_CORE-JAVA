d1e87f5e2ff55dc32f086824adcd1cb72dacf513
dhine <dhine@Snega>
2023-02-23 15:14:53 +0530

    Day01 Code

---
+++ Day01/src/WelcomeProject.java
@@ -0,0 +1,11 @@
+
+public class WelcomeProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to C2TC Program");
+		System.out.println("hello");
+
+	}
+
+}
